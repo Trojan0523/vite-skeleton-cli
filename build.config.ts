615091b83ec7d628da5a5f764fb2f8706b1bcdf3
Trojan0523 <1153532327@qq.com>
2022-10-21 22:48:46 +0800

    feat: unbuild to bin file

---
+++ build.config.ts
@@ -0,0 +1,12 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    './main'
+  ],
+  rollup: {
+    inlineDependencies: true,
+  },
+  declaration: true,
+  clean: true,
+})
